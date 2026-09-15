1d990cbaf3fbcc7d38be52ff1ff78a8c83d31a15
shroompudd-lang <shroompudd@gmail.com>
2026-09-15 01:13:41 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -93,8 +93,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
-      secondary: "#2F3136",
+      primary: "#C21E56",
+      secondary: "#000000",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
